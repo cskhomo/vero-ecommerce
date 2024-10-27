7d641dd5a4ae0caf5e100a355aa9a97aadf5c771
CS Khomo <164577349+cskhomo@users.noreply.github.com>
2024-10-27 14:03:15 +0200

    Fetch spreadsheet data onto the front-end

--- 3.implementation/sheety/script.js
+++ 3.implementation/sheety/script.js
@@ -1,1 +1,17 @@
-// 
+const packsURL = 'https://api.sheety.co/f49d308f0aefb77919c5cfb06b9504ed/catalogue/packs';
+
+async function fetchData(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    console.log(data.packs)
+    displayData(data.packs);
+}
+
+function displayData(itemList) {
+
+    const productDiv = document.querySelector(".product");
+    productDiv.innerHTML = '';
+}
+
+fetchData(packsURL);
