4ca0115a3c7910a32ef8d65026754e19a4cb24ce
CS Khomo <164577349+cskhomo@users.noreply.github.com>
2024-10-27 17:19:08 +0200

    Implement party packs CMS

--- 3.implementation/sheety/script.js
+++ 3.implementation/sheety/script.js
@@ -1,17 +1,42 @@
+// const chocolates = 'https://api.sheety.co/f49d308f0aefb77919c5cfb06b9504ed/catalogue/chocolates';
 const packsURL = 'https://api.sheety.co/f49d308f0aefb77919c5cfb06b9504ed/catalogue/packs';
 
 async function fetchData(url) {
     const response = await fetch(url)
     const data = await response.json()
 
-    console.log(data.packs)
     displayData(data.packs);
 }
 
 function displayData(itemList) {
 
-    const productDiv = document.querySelector(".product");
-    productDiv.innerHTML = '';
+    const productList = document.querySelector(".productList");
+    productList.innerHTML = '';
+
+    itemList.forEach(item => {
+        
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+        productList.appendChild(productDiv);
+
+        const imageDiv = document.createElement("div");
+        imageDiv.classList.add("image");
+        productDiv.appendChild(imageDiv);
+
+        const article = document.createElement("article");
+        productDiv.appendChild(article);
+
+        const titleDiv = document.createElement("div");
+        titleDiv.classList.add("title");
+        titleDiv.innerHTML = item.product;
+        article.appendChild(titleDiv);
+
+        const descriptionDiv = document.createElement("div");
+        descriptionDiv.classList.add("description");
+        descriptionDiv.innerHTML = item.description;
+        article.appendChild(descriptionDiv);
+    });
+  
 }
 
 fetchData(packsURL);
